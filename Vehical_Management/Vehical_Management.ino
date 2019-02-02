56ace1f3ce3e17e2866217b14e73c190d2618564
Madhur Dixit <dmadhur6@gmail.com>
2019-02-03 02:26:57 +0530

    Counting the number of times obstacle arrived infront of sensor

--- Vehical_Management/Vehical_Management.ino
+++ Vehical_Management/Vehical_Management.ino
@@ -3,16 +3,21 @@
 int const trigPin = 10;
 int const echoPin = 9;
 int const led = 2;
+long count=0;
+long tk=0;
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(trigPin, OUTPUT); // trig pin will have pulses output
   pinMode(echoPin, INPUT); // echo pin should be input to get pulse width
   pinMode(led, OUTPUT); // led pin is output to control LED lights
+  
 }
 
 void loop()
 {
+  
   // Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
   int duration, distance;
   // Output pulse with 1ms width on trigPin
@@ -22,14 +27,33 @@ void loop()
   // Measure the pulse input in echo pin
   duration = pulseIn(echoPin, HIGH);
   // Distance is half the duration devided by 29.1 (from datasheet)
-  distance = (duration/2) / 29.1;
+  distance = (duration/2) / 74;
   // if distance less than 0.5 meter and more than 0 (0 or less means over range) 
-    if (distance <= 50 && distance >= 0) {
+    if (distance < 5) {
       // LED on
+      count++;
+      
       digitalWrite(led, HIGH);
     } else {
+
+      if(count!=0)
+      { 
+        tk++;
+        Serial.print("\n---------------------");
+        Serial.print('\n');
+        Serial.print(tk);
+        Serial.print('\n');
+        Serial.print("---------------------");
+        
+      }
+        
+
+      
+      count=0;
+      
       // LED off
       digitalWrite(led, LOW);
     }
     delay(60);
+    
 }
